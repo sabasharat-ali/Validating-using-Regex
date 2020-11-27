831cacfc62095856c092d1c206acb5034bfe9daa
Basharat Ali <sabasharat-ali@gmail.com>
2020-11-27 18:43:14 +0500

    first commit

---
+++ CC_Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CC_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex q1 = new Regex("^[a-z A-Z]{1}[a-z A-Z 0-9 _ - .]+[@gmail @yahoo]+[.com .co]+$");
+            Console.WriteLine("string matched:" + q1.IsMatch("sabasharat.ali12@gmail.com"));
+        }
+    }
+}
